a9da299b9be27a62611ac954ba6201dfbab5d138
bound1ess <bound1ess.dev@gmail.com>
2015-02-11 20:47:33 +0600

    Add FileUtility::read.

--- src/Inspector/Utilities/FileUtility.php
+++ src/Inspector/Utilities/FileUtility.php
@@ -7,4 +7,13 @@ class FileUtility
     {
         return file_exists($file);
     }
+
+    public function read(string $file): mixed
+    {
+        if ( ! $this->exists($file)) {
+            return null;
+        }
+
+        return file_get_contents($file);
+    }
 }
--- tests/Inspector/Utilities/FileUtilityTest.php
+++ tests/Inspector/Utilities/FileUtilityTest.php
@@ -13,4 +13,15 @@ class FileUtilityTest extends \TestCase
         expect(this($file->exists(uniqid())))->to_be_false->go();
         expect(this($file->exists(__FILE__)))->to_be_true->go();
     }
+
+    /**
+     * @test
+     */
+    public function it_reads_a_file()
+    {
+        $file = new FileUtility;
+
+        this($file->read(uniqid()))->should_be_null->go();
+        this($file->read(__FILE__))->should_be_ok->go();
+    }
 }
