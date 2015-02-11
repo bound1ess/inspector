d41fcc7c7a8747cbf07e80400f08d05f2a1fc384
bound1ess <bound1ess.dev@gmail.com>
2015-02-11 21:35:40 +0600

    Add DirUtility::getFiles.

--- src/Inspector/Utilities/DirUtility.php
+++ src/Inspector/Utilities/DirUtility.php
@@ -8,4 +8,22 @@ class DirUtility
         // Somewhat weird.
         return file_exists($dir);
     }
+
+    public function getFiles(string $dir, string $suffix = ".php"): mixed
+    {
+        if ( ! $this->exists($dir)) {
+            return null;
+        }
+
+        $iterator = new \RecursiveIteratorIterator(new \RecursiveDirectoryIterator($dir));
+        $files = [];
+
+        foreach ($iterator as $entry) {
+            if ( ! $entry->isDir() and preg_match("/.+\\$suffix$/", $entry->getFilename())) {
+                $files[] = $entry->getRealPath();
+            }
+        }
+
+        return $files;
+    }
 }
--- tests/Inspector/Utilities/DirUtilityTest.php
+++ tests/Inspector/Utilities/DirUtilityTest.php
@@ -13,4 +13,19 @@ class DirUtilityTest extends \TestCase
         this($dir->exists(uniqid()))->should_be_false->go();
         this($dir->exists(__DIR__))->should_be_true->go();
     }
+
+    /**
+     * @test
+     */
+    public function it_lists_a_directory()
+    {
+        $dir = new DirUtility;
+
+        this($dir->getFiles(uniqid()))->should_be_null->go();
+
+        this($dir->getFiles(__DIR__))->should_be_an("array")->go();
+        this($dir->getFiles(__DIR__))->should_not_be_empty->go();
+
+        this($dir->getFiles(__DIR__, ".asdf"))->should_have_length_of(0)->go();
+    }
 }
