ed9c0fd68090f4dc562c6ae55b463e4e670f7be5
bound1ess <bound1ess.dev@gmail.com>
2015-02-12 18:52:45 +0600

    Move initialization code to src/bootstrap

---
+++ src/bootstrap.php
@@ -0,0 +1,15 @@
+<?hh
+
+if ( ! defined("INSPECTOR")) {
+    define("INSPECTOR", __DIR__."/..");
+}
+
+if ( ! defined("INSPECTOR_WD")) {
+    define("INSPECTOR_WD", getcwd());
+}
+
+if (is_readable($autoloader = INSPECTOR."/vendor/autoload.php")) {
+    require $autoloader;
+} else {
+    require INSPECTOR."/../../autoload.php";
+}
