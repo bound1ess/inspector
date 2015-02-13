18b6fa2bd98821b0a30e1a53f666dcb16d033da1
bound1ess <bound1ess.dev@gmail.com>
2015-02-13 19:04:15 +0600

    Test NodeVisitor.

--- src/Inspector/Inspector.php
+++ src/Inspector/Inspector.php
@@ -1,5 +1,8 @@
 <?php namespace Inspector;
 
+/**
+ * @codeCoverageIgnore
+ */
 class Inspector
 {
 
---
+++ tests/Inspector/NodeVisitorTest.php
@@ -0,0 +1,28 @@
+<?php namespace Inspector;
+
+class NodeVisitorTest extends \TestCase
+{
+
+    /**
+     * @test
+     */
+    public function it_returns_the_nodes_in_the_right_order()
+    {
+        $visitor = new NodeVisitor("foo");
+
+        $result = $visitor->leaveNode(\Mockery::namedMock("Continue_", "PhpParser\Node"));
+        this($result)->should_have_length_of(2)->go();
+        this($result)->should_be_an("array")->go();
+
+        $result = $visitor->leaveNode(\Mockery::namedMock("Else_", "PhpParser\Node"));
+        this($result)->should_have_length_of(2)->go();
+        this($result)->should_be_an("array")->go();
+
+        $result = $visitor->leaveNode(\Mockery::namedMock("If_", "PhpParser\Node"));
+        this($result)->should_be_of_type("array")->go();
+        this($result)->should_be_of_length(3)->go();
+
+        this($visitor->leaveNode(\Mockery::mock("PhpParser\Node")))
+            ->should_be_an("object")->go();
+    }
+}
